166ead5d64ad14f978fdac2e9c8c4ab83e2096b1
Matías PILI <matias.pili@gruposancristobal.com>
2022-06-16 10:53:07 -0300

    Add problem RomanNumbers

---
+++ HackerRankProblems/RomanNumbers.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems
+{
+    public class RomanNumbers
+    {
+        public static void Solve()
+        {
+            Console.Write("Enter list of names with roman number (Ej. 'Luis XI') to sort (without spaces and separate by commas): ");
+
+            string values = Console.ReadLine();
+
+            List<string> names = values.Split(',').ToList();
+
+            Console.Write($"Res = {string.Join(", ", SortByNamesAndRoman(names))}");
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Sort list of names with roman number (Ej. 'Luis XI') by names and roman number
+        /// </summary>
+        /// <param name="names">List of names with roman number</param>
+        /// <returns>List of names with roman number</returns>
+        public static List<string> SortByNamesAndRoman(List<string> names) => names.Select(x => x.Split(" ")).OrderBy(x => x[0]).ThenBy(x => RomanToInt(x[1])).Select(x => string.Join(" ", x)).ToList();
+
+        /// <summary>
+        /// Conver Roman Number to decimal
+        /// </summary>
+        /// <param name="roman">Roman number repesentation</param>
+        /// <returns>Decimal number</returns>
+        public static int RomanToInt(string roman)
+        {
+            int result = 0;
+            Dictionary<char, int> valueDictionary = new()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 },
+            };
+
+
+            for (int i = 0; i < roman.Length; i++)
+            {
+                char romanChar = roman[i];
+                int n = valueDictionary[romanChar];
+
+                if (i + 1 < roman.Length && valueDictionary[roman[i + 1]] > valueDictionary[romanChar])
+                {
+                    result -= n;
+                }
+                else
+                {
+                    result += n;
+                }
+            }
+            return result;
+        }
+    }
+}
