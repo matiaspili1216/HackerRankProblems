cb6cd6e8d5cc29c58df610e9e0af30eec1511b3a
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-09 19:35:07 -0300

    Add Staircase

---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/07.Staircase/StaircasePrepare.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.Staircase
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/staircase/problem
+    /// </summary>
+    public static class StaircasePrepare
+    {
+        public static void Call()
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            StaircaseSolve.Staircase(n);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/07.Staircase/StaircaseSolve.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.Staircase
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/staircase/problem
+    /// </summary>
+    public class StaircaseSolve
+    {
+        public static void Staircase(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"{Repeat(" ", n - i)}{Repeat("#", i)}");
+            }
+        }
+
+        private static string Repeat(string s, int len)
+        {
+            string result = "";
+            for (int i = 0; i < len; i++)
+            {
+                result += s;
+            }
+            return result;
+        }
+    }
+}
