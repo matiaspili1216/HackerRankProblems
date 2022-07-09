94c849e78a4cd2179c103be055fb1af54d3c761f
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-09 19:57:16 -0300

    Add MiniMaxSum

---
+++ HackerRankProblems/Problem Solving/Algorithms/01.Warmup/08.MiniMaxSum/MiniMaxSumPrepare.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.MiniMaxSum
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/mini-max-sum/problem
+    /// </summary>
+    public static class MiniMaxSumPrepare
+    {
+        public static void Call()
+        {
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+            MiniMaxSumSolve.MiniMaxSum(arr);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/01.Warmup/08.MiniMaxSum/MiniMaxSumSolve.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.MiniMaxSum
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/mini-max-sum/problem
+    /// </summary>
+    public class MiniMaxSumSolve
+    {
+        public static void MiniMaxSum(List<int> arr)
+        {
+            List<long> allSum = new List<long>();
+            long sum = arr.Select(x => long.Parse(x.ToString())).Sum();
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                allSum.Add(sum - arr.ElementAt(i));
+            }
+
+            Console.WriteLine($"{allSum.Min()} {allSum.Max()}");
+        }
+    }
+}
