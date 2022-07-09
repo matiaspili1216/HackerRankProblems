9fbffe114c8914781c802be78b577905c45be45f
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-09 17:47:23 -0300

    Add AVeryBigSum

---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/AVeryBigSum/AVeryBigSumPrepare.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.AVeryBigSum
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/a-very-big-sum/problem
+    /// </summary>
+    public class AVeryBigSumPrepare
+    {
+        public void Call()
+        {
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+            long result = AVeryBigSumSolve.AVeryBigSum(ar);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/AVeryBigSum/AVeryBigSumSolve.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.AVeryBigSum
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/simple-array-sum/problem
+    /// </summary>
+    public class AVeryBigSumSolve
+    {
+        public static long AVeryBigSum(List<long> ar)
+        {
+            return ar.Sum();
+        }
+    }
+}
