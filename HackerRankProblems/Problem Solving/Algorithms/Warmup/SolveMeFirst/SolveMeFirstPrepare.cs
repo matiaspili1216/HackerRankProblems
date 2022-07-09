b0eed842aebe409e151c3e701997668a734f5aa1
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-09 00:14:56 -0300

    Add SolveMeFirstSolve

---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/SolveMeFirst/SolveMeFirstPrepare.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.SolveMeFirst
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/solve-me-first/problem
+    /// </summary>
+    public class SolveMeFirstPrepare
+    {
+        int val1 = Convert.ToInt32(Console.ReadLine());
+        int val2 = Convert.ToInt32(Console.ReadLine());
+        int sum = SolveMeFirst.Solve(val1, val2);
+        Console.WriteLine(sum);
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/SolveMeFirst/SolveMeFirstSolve.cs
@@ -0,0 +1,14 @@
+﻿namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.SolveMeFirst
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/solve-me-first/problem
+    /// </summary>
+    public class SolveMeFirstSolve
+    {
+        static int Solve(int a, int b)
+        {
+            // Hint: Type return a+b; below  
+            return a + b;
+        }
+    }
+}
