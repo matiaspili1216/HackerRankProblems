d29cb868e36461e4616d9879c7ed883ae5002a5b
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-09 00:32:10 -0300

    Add CompareTheTriplets

---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/03.CompareTheTriplets/CompareTheTripletsPrepare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.CompareTheTriplets
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/simple-array-sum/problem
+    /// </summary>
+    public class CompareTheTripletsPrepare
+    {
+        public void Call()
+        {
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+            List<int> result = CompareTheTripletsSolve.CompareTriplets(a, b);
+
+            Console.WriteLine(string.Join(" ", result));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/03.CompareTheTriplets/CompareTheTripletsSolve.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.CompareTheTriplets
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/simple-array-sum/problem
+    /// </summary>
+    public class CompareTheTripletsSolve
+    {
+        public static List<int> CompareTriplets(List<int> a, List<int> b)
+        {
+            int pa = 0;
+            int pb = 0;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                if(a.ElementAt(i) > b.ElementAt(i)){
+                    pa++;
+                }
+                else if(a.ElementAt(i) < b.ElementAt(i))
+                {
+                    pb++;
+                }
+            }
+
+            return new List<int>() { pa, pb };
+        }
+    }
+}
