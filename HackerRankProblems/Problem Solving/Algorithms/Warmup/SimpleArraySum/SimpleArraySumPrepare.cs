ba009b8b795aed8dd2500edb80846e4932ca9695
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-09 00:21:08 -0300

    Add SimpleArraySum

---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/SimpleArraySum/SimpleArraySumPrepare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.SimpleArraySum
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/solve-me-first/problem
+    /// </summary>
+    public class SimpleArraySumPrepare
+    {
+        public void Call()
+        {
+
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+            int result = SimpleArraySumSolve.SimpleArraySum(ar);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/SimpleArraySum/SimpleArraySumSolve.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.SimpleArraySum
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/solve-me-first/problem
+    /// </summary>
+    public class SimpleArraySumSolve
+    {
+        public static int SimpleArraySum(List<int> ar)
+        {
+            return ar.Sum();
+        }
+    }
+}
