5b18a37a7126dada74f21f16aec899d8c895f463
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-09 18:40:14 -0300

    Add DiagonalDifference

---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/05.DiagonalDifference/DiagonalDifferencePrepare.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.DiagonalDifference
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/diagonal-difference/problem
+    /// </summary>
+    public static class DiagonalDifferencePrepare
+    {
+        public static void Call()
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+
+            int result = DiagonalDifferenceSolve.DiagonalDifference(arr);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/05.DiagonalDifference/DiagonalDifferenceSolve.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.DiagonalDifference
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/simple-array-sum/problem
+    /// </summary>
+    public class DiagonalDifferenceSolve
+    {
+        public static int DiagonalDifference(List<List<int>> arr)
+        {
+            int max = arr.Count;
+            int xD1 = 0;
+            int yD1 = 0;
+
+            int xD2 = 0;
+            int yD2 = max -1;
+
+            int sumD1 = 0;
+            int sumD2 = 0;
+
+            for (int x = 0; x < max; x++)
+            {
+                var row = arr.ElementAt(x);
+
+                for (int y = 0; y < max; y++)
+                {
+                    if(x == xD1 && y == yD1) { 
+                        sumD1 += row.ElementAt(y);
+                        xD1++;
+                        yD1++;
+                    }
+
+                    if (x == xD2 && y == yD2)
+                    {
+                        sumD2 += row.ElementAt(y);
+                        xD2++;
+                        yD2--;
+                    }
+                }
+            }
+
+            return Math.Abs(sumD1 - sumD2);
+        }
+    }
+}
