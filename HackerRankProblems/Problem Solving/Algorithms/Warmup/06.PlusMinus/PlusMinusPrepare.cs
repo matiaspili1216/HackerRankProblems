234b3e944668bfd2c2aad0fc461d43183b6b9852
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-09 18:55:48 -0300

    Add PlusMinus

---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/06.PlusMinus/PlusMinusPrepare.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.PlusMinus
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/plus-minus/problem
+    /// </summary>
+    public static class PlusMinusPrepare
+    {
+        public static void Call()
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            PlusMinusSolve.PlusMinus(arr);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/Warmup/06.PlusMinus/PlusMinusSolve.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.PlusMinus
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/plus-minus/problem
+    /// </summary>
+    public class PlusMinusSolve
+    {
+        public static void PlusMinus(List<int> arr)
+        {
+            int t = arr.Count;
+            float cP = arr.Count(x => x > 0);
+            float c0 = arr.Count(x => x == 0);
+            float cN = arr.Count(x => x < 0);
+
+            Console.WriteLine($"{cP / t:0.000000}");
+            Console.WriteLine($"{cN / t:0.000000}");
+            Console.WriteLine($"{c0 / t:0.000000}");
+        }
+    }
+}
