c30aa355c5ae6ec7d7a51552d25442c506221a14
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-10 13:03:26 -0300

    TimeConversion

---
+++ HackerRankProblems/Problem Solving/Algorithms/01.Warmup/10.TimeConversion/TimeConversionPrepare.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.TimeConversion
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/time-conversion/problem
+    /// </summary>
+    public static class TimeConversionPrepare
+    {
+        public static void Call()
+        {
+            string s = Console.ReadLine();
+
+            string result = TimeConversionSolve.TimeConversion(s);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/01.Warmup/10.TimeConversion/TimeConversionSolve.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.TimeConversion
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/time-conversion/problem
+    /// </summary>
+    public class TimeConversionSolve
+    {
+        public static string TimeConversion(string time)
+        {
+            return $"{DateTime.Parse(time):HH:mm:ss}";
+        }
+    }
+}
