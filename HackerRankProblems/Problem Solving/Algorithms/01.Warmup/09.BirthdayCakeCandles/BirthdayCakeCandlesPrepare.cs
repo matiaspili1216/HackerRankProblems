d3cdf920b38f21bff84a5e70eadafedb6345624c
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-10 12:52:35 -0300

    Add BirthdayCakeCandles

---
+++ HackerRankProblems/Problem Solving/Algorithms/01.Warmup/09.BirthdayCakeCandles/BirthdayCakeCandlesPrepare.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.BirthdayCakeCandles
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+    /// </summary>
+    public static class BirthdayCakeCandlesPrepare
+    {
+        public static void Call()
+        {
+            int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+            int result = BirthdayCakeCandlesSolve.BirthdayCakeCandles(candles);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/01.Warmup/09.BirthdayCakeCandles/BirthdayCakeCandlesSolve.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Warmup.BirthdayCakeCandles
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+    /// </summary>
+    public class BirthdayCakeCandlesSolve
+    {
+        public static int BirthdayCakeCandles(List<int> candles)
+        {
+            int max = candles.Max();
+            return candles.Count(x => x == max);
+        }
+    }
+}
