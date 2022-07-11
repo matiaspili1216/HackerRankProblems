438a1dd205eacfadfbb67dc5f9c393abc1c39e18
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-11 15:01:00 -0300

    Add NumberLineJumps

---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/03.NumberLineJumps/NumberLineJumpsPrepare.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.NumberLineJumps
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/kangaroo/problem
+    /// </summary>
+    public class NumberLineJumpsPrepare
+    {
+        public static void Call()
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int x1 = Convert.ToInt32(firstMultipleInput[0]);
+
+            int v1 = Convert.ToInt32(firstMultipleInput[1]);
+
+            int x2 = Convert.ToInt32(firstMultipleInput[2]);
+
+            int v2 = Convert.ToInt32(firstMultipleInput[3]);
+
+            string result = NumberLineJumpsSolve.Kangaroo(x1, v1, x2, v2);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/03.NumberLineJumps/NumberLineJumpsSolve.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.NumberLineJumps
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/kangaroo/problem
+    /// </summary>
+    public class NumberLineJumpsSolve
+    {
+        public static string Kangaroo(int x1, int v1, int x2, int v2)
+        {
+            if(v2 > v1 || v1 == v2) { return "NO"; }
+            else
+            {
+                float jumps = (x2 - x1) / (float)(v1 - v2);
+
+                return jumps == Math.Round(jumps, 0) ? "YES" : "NO";
+            }
+        }
+    }
+}
