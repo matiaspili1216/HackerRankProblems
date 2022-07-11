b115bcf04fed244fc4ea50574327021c1fdcd6ad
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-11 10:37:56 -0300

    Add GradingStudents problem

---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/01.GradingStudents/GradingStudentsSolve.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.GradingStudents
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/grading/problem
+    /// </summary>
+    public class GradingStudentsSolve
+    {
+        public static List<int> Solve(List<int> grades)
+        {
+            List<int> result = new List<int>();
+
+            foreach (var grade in grades)
+            {
+                if(grade < 38) { result.Add(grade); }
+                else
+                {
+                    int diff = grade % 5;
+
+                    if(diff == 0) { result.Add(grade); }
+                    else if(diff > 2) { result.Add(grade + 5 - diff); }
+                    else { result.Add(grade); }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/01.GradingStudents/GradingStudentstPrepare.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.GradingStudents
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/grading/problem
+    /// </summary>
+    public class GradingStudentsPrepare
+    {
+        public static void Call()
+        {
+            int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> grades = new List<int>();
+
+            for (int i = 0; i < gradesCount; i++)
+            {
+                int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+                grades.Add(gradesItem);
+            }
+
+            List<int> result = GradingStudentsSolve.Solve(grades);
+
+            Console.WriteLine(string.Join("\n", result));
+            Console.ReadLine();
+        }
+    }
+}
