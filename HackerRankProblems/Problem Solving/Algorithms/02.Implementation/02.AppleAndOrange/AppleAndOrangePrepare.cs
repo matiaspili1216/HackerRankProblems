ed8634e4820bea4d911c2e6f956a1cd43c9a921d
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-11 11:55:34 -0300

    Add AppleAndOrange

---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/02.AppleAndOrange/AppleAndOrangePrepare.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.AppleAndOrange
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/apple-and-orange/problem
+    /// </summary>
+    public class AppleAndOrangePrepare
+    {
+        public static void Call()
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+            int s = Convert.ToInt32(firstMultipleInput[0]);
+
+            int t = Convert.ToInt32(firstMultipleInput[1]);
+
+            string[] secondMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int a = Convert.ToInt32(secondMultipleInput[0]);
+
+            int b = Convert.ToInt32(secondMultipleInput[1]);
+
+            string[] thirdMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int m = Convert.ToInt32(thirdMultipleInput[0]);
+
+            int n = Convert.ToInt32(thirdMultipleInput[1]);
+
+            List<int> apples = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(applesTemp => Convert.ToInt32(applesTemp)).ToList();
+
+            List<int> oranges = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(orangesTemp => Convert.ToInt32(orangesTemp)).ToList();
+
+            AppleAndOrangeSolve.CountApplesAndOranges(s, t, a, b, apples, oranges);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/02.AppleAndOrange/AppleAndOrangeSolve.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.AppleAndOrange
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/apple-and-orange/problem
+    /// </summary>
+    public class AppleAndOrangeSolve
+    {
+        public static void CountApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+        {
+            Console.WriteLine(apples.Count(ap => Between(a + ap, s, t)));
+            Console.WriteLine(oranges.Count(op => Between(b + op, s, t)));
+        }
+
+        /// <summary>
+        /// Given a value, it calculates if this value is between a minimum and a maximum.
+        /// </summary>
+        /// <param name="v">Value to calculate</param>
+        /// <param name="min">Minimum value</param>
+        /// <param name="max">Maximum value</param>
+        /// <returns>true if value is between a minimum and a maximum</returns>
+        private static bool Between(int v, int min, int max) => min <= v && max >= v;
+    }
+}
