d15de0cc575a85bd32bb5a928e951148a2f1899c
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-12 10:03:33 -0300

    Add BetweenTwoSets

---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/04.BetweenTwoSets/BetweenTwoSetsPrepare.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.BetweenTwoSets
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/between-two-sets/problem
+    /// </summary>
+    public class BetweenTwoSetsPrepare
+    {
+        public static void Call()
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int m = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            List<int> brr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(brrTemp => Convert.ToInt32(brrTemp)).ToList();
+
+            int result = BetweenTwoSetsSolve.GetTotalX(arr, brr);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/04.BetweenTwoSets/BetweenTwoSetsSolve.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.BetweenTwoSets
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/between-two-sets/problem
+    /// </summary>
+    public class BetweenTwoSetsSolve
+    {
+        public static int GetTotalX(List<int> a, List<int> b)
+        {
+            var comunFac = ComunFactors(b);
+
+            int result = 0;
+
+            foreach (var f in comunFac)
+            {
+                if(a.All(x => f % x == 0)) { result++; }
+            }
+
+            return result;
+        }
+
+        private static List<int> ComunFactors(List<int> values)
+        {
+            List<int> facs = new List<int>();
+
+            foreach (var value in values.OrderByDescending(x => x))
+            {
+                var f = Factors(value);
+                if (facs.Any())
+                {
+                    var newFacts = f.Where(x => facs.Contains(x)).ToList();
+                    facs = newFacts;
+                }
+                else
+                {
+                    facs = f;
+                }
+            }
+
+            return facs;
+        }
+
+        private static List<int> Factors(int value)
+        {
+            List<int> result = new List<int>() { value };
+
+            for (int i = 1; i <= value / 2; i++)
+            {
+                if(value % i == 0) { result.Add(i); }
+            }
+
+            return result;
+        }
+    }
+}
