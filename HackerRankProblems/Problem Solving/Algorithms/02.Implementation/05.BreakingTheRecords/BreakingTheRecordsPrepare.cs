89b286f1fcccca38038fd4087bd09b4d18d3185c
Matías PILI <matias.pili@gruposancristobal.com>
2022-07-12 13:01:56 -0300

    Add BreakingTheRecords

---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/05.BreakingTheRecords/BreakingTheRecordsPrepare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.BreakingTheRecords
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+    /// </summary>
+    public class BreakingTheRecordsPrepare
+    {
+        public static void Call()
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> scores = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(scoresTemp => Convert.ToInt32(scoresTemp)).ToList();
+
+            List<int> result = BreakingTheRecordsSolve.BreakingRecords(scores);
+
+            Console.WriteLine(string.Join(" ", result));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/05.BreakingTheRecords/BreakingTheRecordsSolve.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.BreakingTheRecords
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+    /// </summary>
+    public class BreakingTheRecordsSolve
+    {
+        public static List<int> BreakingRecords(List<int> scores)
+        {
+            int minCount = 0;
+            int maxCount = 0;
+            int min = scores.ElementAt(0);
+            int max = scores.ElementAt(0);
+
+            foreach (var score in scores)
+            {
+                if(score < min)
+                {
+                    minCount++;
+                    min = score;
+                }
+
+                if(score > max)
+                {
+                    maxCount++;
+                    max = score;
+                }
+            }
+
+            return new List<int>() { maxCount, minCount};
+        }
+    }
+}
