d36639156d86f0ce75223b13a1e5f2ec7091190b
Matías PILI <matias.pili@gruposancristobal.com>
2024-08-19 20:56:04 -0300

    Add BonAppetit problem

---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/956.BonAppetit/BonAppetitPrepare.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.BonAppetit
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/bon-appetit/problem
+    /// </summary>
+    public class BonAppetitPrepare
+    {
+        public static void Call()
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int k = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> bill = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(billTemp => Convert.ToInt32(billTemp)).ToList();
+
+            int b = Convert.ToInt32(Console.ReadLine().Trim());
+
+            BonAppetitSolve.Solve(bill, k, b);
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/956.BonAppetit/BonAppetitSolve.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.BonAppetit
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/bon-appetit/problem
+    /// </summary>
+    public class BonAppetitSolve
+    {
+        /*
+         * Complete the 'bonAppetit' function below.
+         *
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY bill
+         *  2. INTEGER k
+         *  3. INTEGER b
+         */
+
+        public static void Solve(List<int> bill, int k, int b)
+        {
+            int exclude = bill.ElementAt(k);
+            bill.Remove(exclude);
+            int amount = bill.Sum() / 2;
+
+            if (amount == b)
+            {
+                Console.WriteLine("Bon Appetit");
+            }
+            else
+            {
+                Console.WriteLine(b - amount);
+            }
+        }
+    }
+}
