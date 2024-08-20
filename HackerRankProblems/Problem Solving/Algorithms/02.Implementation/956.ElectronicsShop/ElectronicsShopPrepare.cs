cdd4dd40f208103fe054abe0d10164277c97a0ae
Matías PILI <matias.pili@gruposancristobal.com>
2024-08-20 12:30:20 -0300

    Add ElectronicsShop problem

---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/956.ElectronicsShop/ElectronicsShopPrepare.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.ElectronicsShop
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/electronics-shop/problem
+    /// </summary>
+    public class ElectronicsShopPrepare
+    {
+        public static void Call()
+        {
+            string[] bnm = Console.ReadLine().Split(' ');
+
+            int b = Convert.ToInt32(bnm[0]);
+
+            int n = Convert.ToInt32(bnm[1]);
+
+            int m = Convert.ToInt32(bnm[2]);
+
+            int[] keyboards = Array.ConvertAll(Console.ReadLine().Split(' '), keyboardsTemp => Convert.ToInt32(keyboardsTemp))
+            ;
+
+            int[] drives = Array.ConvertAll(Console.ReadLine().Split(' '), drivesTemp => Convert.ToInt32(drivesTemp))
+            ;
+            /*
+             * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+             */
+
+            int result  = ElectronicsShopSolve.Solve(keyboards, drives, b);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/956.ElectronicsShop/ElectronicsShopSolve.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.ElectronicsShop
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/electronics-shop/problem
+    /// </summary>
+    public class ElectronicsShopSolve
+    {
+        /*
+         * Complete the 'bonAppetit' function below.
+         *
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY bill
+         *  2. INTEGER k
+         *  3. INTEGER b
+         */
+
+        public static int Solve(int[] keyboards, int[] drives, int b)
+        {
+            List<int> sums = new List<int>();
+
+            foreach (int i in keyboards.Where(x => x < b))
+            {
+                foreach (int j in drives.Where(x => x < b))
+                {
+                    sums.Add(i + j);
+                }
+            }
+
+            var l = sums.Where(x => x <= b);
+
+            return l.Any() ? l.Max() : -1;
+        }
+    }
+}
