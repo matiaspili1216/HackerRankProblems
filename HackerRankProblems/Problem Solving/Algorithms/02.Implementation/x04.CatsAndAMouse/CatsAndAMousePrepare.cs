b798a17c9f0041b4cf1f6208ef407defe9d8145f
Matías PILI <matias.pili@gruposancristobal.com>
2024-08-20 15:12:53 -0300

    Add CatsAndAMouse problem

---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/x04.CatsAndAMouse/CatsAndAMousePrepare.cs
@@ -0,0 +1,35 @@
+﻿using HackerRankProblems.Problem_Solving.Algorithms.Implementation.ElectronicsShop;
+
+using System;
+using System.IO;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.CatsAndAMouse
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
+    /// </summary>
+    public class CatsAndAMousePrepare
+    {
+        public static void Call()
+        {
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string[] xyz = Console.ReadLine().Split(' ');
+
+                int x = Convert.ToInt32(xyz[0]);
+
+                int y = Convert.ToInt32(xyz[1]);
+
+                int z = Convert.ToInt32(xyz[2]);
+
+                string result = ElectronicsShopSolve.Solve(x, y, z);
+
+                Console.WriteLine(result);
+            }
+
+            
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/x04.CatsAndAMouse/CatsAndAMouseSolve.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.CatsAndAMouse
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
+    /// </summary>
+    public class CatsAndAMouseSolve
+    {
+        private static readonly string CatA = "Cat A";
+        private static readonly string CatB = "Cat B";
+        private static readonly string MouseC = "Mouse C";
+
+        public static string Solve(int x, int y, int z)
+        {
+            int dA = Math.Abs(z - x);
+            int dB = Math.Abs(z - y);
+
+            return dA == dB ? MouseC : dA > dB ? CatB : CatA;
+        }
+    }
+}
