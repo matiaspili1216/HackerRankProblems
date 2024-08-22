d3f122ffc0e24b58d94ab5274b6ae02caf3137e2
Matías PILI <matias.pili@gruposancristobal.com>
2024-08-21 23:53:25 -0300

    Add DesignerPdfViewer problem

---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/x09.DesignerPdfViewer/DesignerPdfViewerPrepare.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.DesignerPdfViewer
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+    /// </summary>
+    public class DesignerPdfViewerPrepare
+    {
+        public static void Call()
+        {
+            List<int> h = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(hTemp => Convert.ToInt32(hTemp)).ToList();
+
+            string word = Console.ReadLine();
+
+            int result = DesignerPdfViewerSolve.Solve(h, word);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/x09.DesignerPdfViewer/DesignerPdfViewerSolve.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.DesignerPdfViewer
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+    /// </summary>
+    public class DesignerPdfViewerSolve
+    {
+
+
+        public static int Solve(List<int> h, string word)
+        {
+            return word.Select(w => h["abcdefghijklmnopqrstuvwxyz".IndexOf(w)]).Max() * word.Length;
+        }
+    }
+}
