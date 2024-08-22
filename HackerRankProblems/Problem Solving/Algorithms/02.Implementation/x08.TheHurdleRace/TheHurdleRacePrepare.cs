e639725c13cc5aa82b919ffad83749d9692be7b4
Matías PILI <matias.pili@gruposancristobal.com>
2024-08-21 23:34:39 -0300

    Add TheHurdleRace problem

---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/x08.TheHurdleRace/TheHurdleRacePrepare.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.TheHurdleRace
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/the-hurdle-race/problem
+    /// </summary>
+    public class TheHurdleRacePrepare
+    {
+        public static void Call()
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int k = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> height = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(heightTemp => Convert.ToInt32(heightTemp)).ToList();
+
+            int result = TheHurdleRaceSolve.Solve(k, height);
+
+            Console.WriteLine(string.Join("\n", result));
+        }
+    }
+}
---
+++ HackerRankProblems/Problem Solving/Algorithms/02.Implementation/x08.TheHurdleRace/TheHurdleRaceSolve.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankProblems.Problem_Solving.Algorithms.Implementation.TheHurdleRace
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/the-hurdle-race/problem
+    /// </summary>
+    public class TheHurdleRaceSolve
+    {
+        public static int Solve(int k, List<int> height) => Math.Max(0, height.Max() - k);
+    }
+}
